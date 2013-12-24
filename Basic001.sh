557e63578581d81878f65312f6ada7d3e64a8590
Aluísio Macedo <ajmacedo@AthlonXP_XUbuntu.(none)>
2013-12-24 10:55:59 -0200

    First Commit

---
+++ Basic001.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+echo "Tenho bons links aqui dentro!!!"
+
+for i in {1..5}
+do
+  echo "Loop ativo $i ..."
+done
+
+
+# http://tldp.org/LDP/Bash-Beginners-Guide/html/Bash-Beginners-Guide.html#chap_01
+# http://www.infowester.com/linuxcron.php
